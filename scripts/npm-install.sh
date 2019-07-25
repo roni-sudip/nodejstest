a18ac16a8aa6100e69081a1bb9ca4074473da3ab
ronisudip@gmail.com <ronisudip@gmail.com>
2019-07-25 13:37:34 +0600

    scripts/npm-install.sh

--- scripts/npm-install.sh
+++ scripts/npm-install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /var/nodeapp
+yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
+sudo yum install nodejs
 npm install
 npm install -g forever
